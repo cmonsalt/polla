e697a39653ab2d111178b016acfea178914ecaf6
cmonsalt <cristiantm12@gmail.com>
2024-04-22 20:00:54 -0500

    Continuación15

--- resources/js/components/TicketsSection.jsx
+++ resources/js/components/TicketsSection.jsx
@@ -15,9 +15,7 @@ function TicketsSection() {
         fetch("/api/marcadores")
             .then((response) => response.json())
             .then((data) => setMarcadores(data))
-            .catch((error) =>
-                console.error("Error al cargar los marcadores:", error)
-            );
+            .catch((error) => console.error("Error al cargar los marcadores:", error));
     }, []);
 
     const getMarcadorWeightClass = (marcador) => {
@@ -38,7 +36,7 @@ function TicketsSection() {
             show: true,
             content: content,
             x: event.clientX,
-            y: event.clientY + 20, // Offset to position below cursor
+            y: event.clientY + 20, // Offset para posicionar debajo del cursor
         });
     };
 
@@ -60,31 +58,26 @@ function TicketsSection() {
                             position: "fixed",
                             top: `${tooltip.y}px`,
                             left: `${tooltip.x}px`,
-                            padding: "10px 20px", // Valores de padding reducidos para dar más espacio al texto
+                            padding: "10px 20px",
                             backgroundColor: "#333333",
                             color: "white",
                             borderRadius: "5px",
                             zIndex: 9999,
-                            display: "block", // Asegura que siempre se muestre cuando deba estar visible
-                            minWidth: "500px", // Un ancho mínimo para acomodar el contenido
-                            maxWidth: "500px", // Un ancho máximo para prevenir que el tooltip sea demasiado grande
+                            display: "block",
+                            minWidth: "500px",
+                            maxWidth: "500px",
                             height: "auto",
-                            whiteSpace: "normal", // Permite que el texto se ajuste al cambiar de línea
-                            overflowWrap: "break-word", // Asegura que las palabras se rompan si son demasiado largas para la línea
+                            whiteSpace: "normal",
+                            overflowWrap: "break-word",
                         }}
-                    >
-                        {tooltip.content}
-                    </div>
+                        dangerouslySetInnerHTML={{ __html: tooltip.content }}
+                    />
                 )}
                 {marcadores.map((marcador, index) => (
                     <div
                         key={index}
-                        className={`circle ${
-                            !marcador.status ? "gray" : ""
-                        } ${getMarcadorWeightClass(marcador)}`}
-                        onMouseEnter={(e) =>
-                            showTooltip(e, getTooltipContent(marcador))
-                        }
+                        className={`circle ${!marcador.status ? "gray" : ""} ${getMarcadorWeightClass(marcador)}`}
+                        onMouseEnter={(e) => showTooltip(e, getTooltipContent(marcador))}
                         onMouseLeave={hideTooltip}
                     >
                         {marcador.marcador}
@@ -94,19 +87,18 @@ function TicketsSection() {
         </div>
     );
 }
+
 function getTooltipContent(marcador) {
     switch (marcador.peso) {
         case "Alto":
-            return "🌟 ¡Gran oportunidad! Este es uno de los marcadores más comunes en el fútbol. Si se te asigna, estarás a un paso de ganar el premio mayor. ¡Suerte!";
+            return `<span class="alto"></span> <strong>¡Gran oportunidad!</strong> Este es uno de los marcadores más comunes en el fútbol.<br/>Si se te asigna, estarás a un paso de ganar el premio mayor. ¡Suerte! <br>${marcador.status === 1 ? "✅ Disponible Para Entregar" : "❌ Entregado"}`;
         case "Medio":
-            return "🎯 Este resultado no es muy común, pero si coincide con el segundo lugar determinado por la organización, ganarás un cupón para participar gratis en el próximo evento.";
+            return `🌟 <strong>Este resultado no es muy común,</strong> pero si coincide con el segundo lugar determinado por la organización, ganarás un cupón para participar gratis en el próximo evento.Estado: ${marcador.status === 1 ? "✅ Disponible Para Entregar" : "❌ Entregado"}`;
         case "Bajo":
-            return "🍀 Poco probable, pero no imposible. Si este resultado coincide con el segundo lugar determinado por la organización, también ganarás un cupón para el próximo evento. ¡Intenta tu suerte!";
+            return `🌟 <strong>Poco probable, pero no imposible.</strong> Si este resultado coincide con el segundo lugar determinado por la organización, también ganarás un cupón para el próximo evento.Estado: ${marcador.status === 1 ? "✅ Disponible Para Entregar" : "❌ Entregado"}`;
         default:
-            return "🍀 Marcador disponible. Si tu compra es exitosa, se te asignará uno al azar. ¡Que la suerte esté de tu lado!";
+            return `Marcador disponible. Si tu compra es exitosa, se te asignará uno al azar. ¡Que la suerte esté de tu lado!</div><div>Estado: ${marcador.status === 1 ? "✅ Disponible Para Entregar" : "❌ Entregado"}</div>`;
     }
 }
-
-
 
 export default TicketsSection;
