753009ea83ee0bc61aa34071dfbbe2f70b1af22a
cmonsalt <cristiantm12@gmail.com>
2024-05-08 17:51:37 -0500

    footer2

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Footer({onOpenPoliticaModal}) {
     return (
         <footer className="text-white mt-5">
-            <div className="container py-4">
-                <div className="row">
-                    <div className="col-md-12 text-center">
+           
                         <h5 className="footer-text">GolazoSorteos</h5>
                         <p className="footer-text">
                             &copy; 2024 GolazoSorteos, Inc. Todos los derechos reservados.
@@ -14,9 +12,7 @@ export default function Footer({onOpenPoliticaModal}) {
                             Política de Privacidad
                             
                         </a>
-                    </div>
-                </div>
-            </div>
+              
         </footer>
     );
 }
